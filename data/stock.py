0d1c7b76b12d6765c7f9d5cde9d0b928e7942744
jiancheng pei <jpei@sonicwall.com>
2020-09-08 15:20:34 +0800

    data: commit Stock class

---
+++ data/stock.py
@@ -0,0 +1,32 @@
+
+'''
+define stock baic information
+
+'''
+def class Stock(Object):
+   
+    def __init__(self, id, name, area, stockEx):
+        self.name = name
+        self.id = id
+        self.price = 0
+        self.lastTradeTime = ""
+        self.area = ""
+        self.stockEx = ""
+        
+    def getID(self):
+        return self.id
+    def getName(self):
+        return self.name
+        
+    def getPrice(self):
+        return self.price
+    def setPrice(self, price):
+        self.price = price
+    
+    def getLastTradeTime(self):
+        return self.lastTradeTime
+    def setLastTradeTime(self, time):
+        return self.lastTradeTime = lastTradeTime
+    
+        
+    
